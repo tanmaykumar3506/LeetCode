196260b8810474dbbcb747f2ad310c677d5f5903
Tanmay Kumar <149177698+tanmaykumar3506@users.noreply.github.com>
2026-01-30 22:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (91.95%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // int n=nums.size();
+        // int i = n-2;
+        // if ()
+        next_permutation(nums.begin(),nums.end());
+    }
+};
